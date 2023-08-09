4a180bf5b164a42f518d67e11ba2f2dae78e2c93
Sergi Triado <sergi.triadom@gmail.com>
2023-08-09 11:52:34 +0200

    Bootstrap & FA

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Navbar(){
     const [mode, setMode] = useState('light')
 
     return (
-            <h1 className={`navbar ${mode}`}>
-                Hola Mundo
-            </h1>
+        <>
+            <div className={`navbar ${mode}`}>
+                <h1 className="title">
+                    PokéDex
+                </h1>
+                <FontAwesomeIcon icon={mode === 'light' ? faMoon : faSun} 
+                    onClick={() => setMode(mode === 'light' ? 'dark' : 'light')} />
+
+            </div>
+        </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './Navbar/Navbar.jsx'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
