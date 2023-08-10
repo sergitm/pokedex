ff3312d3112508bb4b585a80d7ac33044b421972
Sergi Triado <sergi.triadom@gmail.com>
2023-08-10 16:05:40 +0200

    Navbar styles and modes

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -2,21 +2,29 @@ import { useState } from 'react'
 import './Navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
+import logo from '../assets/pokedex-logo.png'
 
 
 export default function Navbar(){
-    const [mode, setMode] = useState('light')
+    const [mode, setMode] = useState(localStorage.getItem('mode') || 'light')
+    const textMode = (mode === 'light') ? 'dark' : 'light'
 
     return (
         <>
-            <div className={`navbar ${mode}`}>
-                <h1 className="title">
-                    PokéDex
-                </h1>
-                <FontAwesomeIcon icon={mode === 'light' ? faMoon : faSun} 
-                    onClick={() => setMode(mode === 'light' ? 'dark' : 'light')} />
-
-            </div>
+            <nav className={`navbar navbar-${mode} bg-${mode}`}>
+                <div className="container-fluid">
+                    <a className='navbar-brand icon' href='#'><img src={logo} alt="" width="30" height="24" /></a>
+                    <span className={`h1 title text-${textMode}`}>PokéDex</span>
+                    <FontAwesomeIcon className={`icon-${mode} mx-5`} icon={mode === 'light' ? faMoon : faSun} 
+                        onClick={() => toggleMode()} />
+                </div>
+            </nav>
         </>
     )
+
+    function toggleMode(){
+        const newMode = mode === 'light' ? 'dark' : 'light'
+        setMode(newMode)
+        localStorage.setItem('mode', newMode)
+    }
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Navbar from './Navbar/Navbar.jsx'
+import App from './App.jsx'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "./main.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navbar />
+    <App />
   </React.StrictMode>,
 )
