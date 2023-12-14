be79061893596297221815c468a92ed541c398d5
sergitm <sergi.triadom@gmail.com>
2023-12-14 16:59:18 +0100

    FilterBar

--- src/Content/SearchBar.jsx
+++ src/Content/SearchBar.jsx
@@ -1,16 +1,37 @@
 import { connect } from 'react-redux'
+import { useEffect, useState } from 'react';
+import TypeFilter from './TypeFilter';
 
 function SearchBar(props){
+    const [types, setTypes] = useState([]);
     const mode = props.mode
+
+    useEffect(() => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://pokedex-backend.test/api/pokemon/types');
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                setTypes(JSON.parse(xhr.responseText));
+            }
+        };
+        xhr.send();  
+    },[])
 
     return (
         <>
         <div className="container mt-5">
-            <div className="row align-items-center">
+            <div className={`row align-items-center p-2 bg-${mode} rounded rounded-4`}>
                 <div className='col-3'>
                     <div className="input-group">
-                        <input className="form-control " type="text" id="search" placeholder="Search" />
-                        <button className={`btn btn-${mode === 'light' ? 'primary' : 'dark'}`} type="button" id="searchButton">Search</button>
+                        <input className="form-control " type="text" id="search" placeholder="Name or Pokedex #" />
+                        <button className={`btn btn-${mode === 'light' ? 'primary' : 'secondary'}`} type="button" id="searchButton">Search</button>
+                    </div>
+                </div>
+                <div className="col-9">
+                    <div className='btn-group' role='types' aria-label='Pokemon Types radio button group'>
+                        {types ? types.map((type) => (
+                            <TypeFilter type={type} mode={mode} key={type.name} />
+                        )) : <></>}
                     </div>
                 </div>
             </div>
---
+++ src/Content/TypeFilter.jsx
@@ -0,0 +1,14 @@
+
+
+export default function TypeFilter(props) {
+    const typeNameF = props.type.name.charAt(0).toUpperCase() + props.type.name.slice(1);
+    const imgUrl = `./src/assets/types-img/Battrio_${typeNameF}_type.png`;
+
+    return (
+    <>
+        <input type="radio" className="btn-check" name="types" id={props.type.name} autoComplete="off" />
+        <label className={`btn btn-outline-${props.mode === 'light' ? 'primary' : 'secondary'} text-white`} htmlFor={props.type.name} title={typeNameF}>
+            <img src={imgUrl} alt={typeNameF} style={{width: '100%', height: '100%', objectFit: 'contain'}}/>
+        </label>
+    </>);
+}
