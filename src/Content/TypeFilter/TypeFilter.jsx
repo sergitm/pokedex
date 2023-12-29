9fe3250fc3343d08ad60b0ae257fbfc9d86ea743
sergitm <sergi.triadom@gmail.com>
2023-12-29 15:14:49 +0100

    Type Filters 1 & 2 Dropdowns

--- src/Content/TypeFilter/TypeFilter.jsx
+++ src/Content/TypeFilter/TypeFilter.jsx
@@ -22,15 +22,44 @@ export default function TypeFilter(props) {
         return `./src/assets/types-img/Battrio_${typeNameF(type)}_type.png`;
     } 
     
-    return (
-    <div className='btn-group' role='types' aria-label='Pokemon Types radio button group'>
-        { types.length !== 0 ? types.map((type) => (
-            <Fragment key={type.name}>
-                <input type="radio" className="btn-check" name="types" id={type.name} autoComplete="off" />
-                <label className={`btn btn-outline-${props.mode === 'light' ? 'dark' : 'secondary'} text-white btn-type`} htmlFor={type.name} title={typeNameF(type)}>
-                    <img src={imgUrl(type)} alt={typeNameF(type)} style={{width: '100%', height: '100%', objectFit: 'contain'}}/>
-                </label>
-            </Fragment>        
-        )) : <h3 className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}>Loading..</h3>}
+    return (<div className='container row'>
+    <div className="dropdown col text-center">
+        <button className="btn btn-dark dropdown-toggle" type="button" id="type1Dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+            Select Type 1
+        </button>
+        <ul className="dropdown-menu" aria-labelledby="type1Dropdown">
+            <li className="dropdown-header">Type 1</li>
+            <li><hr className="dropdown-divider border-top border-secondary" /></li>
+            <div className='btn-group row' role='types1' aria-label='Pokemon Types radio button group'>
+            { types.length !== 0 ? types.map((type) => (
+                <li key={type.name+"1"} className='col-2'>
+                    <input type="radio" className="btn-check" name="types1" id={type.name+"1"} autoComplete="off" />
+                    <label className={`btn btn-outline-${props.mode === 'light' ? 'dark' : 'secondary'} text-white btn-type`} htmlFor={type.name+"1"} title={typeNameF(type)}>
+                        <img src={imgUrl(type)} alt={typeNameF(type)} style={{width: '100%', height: '100%', objectFit: 'contain'}}/>
+                    </label>
+                </li>
+            )) : <h3 className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}>Loading..</h3>}
+            </div>
+        </ul>
+    </div>
+    <div className="dropdown col text-center">
+        <button className="btn btn-dark dropdown-toggle" type="button" id="type2Dropdown" data-bs-toggle="dropdown" aria-expanded="false">
+            Select Type 2
+        </button>
+        <ul className="dropdown-menu" aria-labelledby="type2Dropdown">
+            <li className="dropdown-header">Type 2</li>
+            <li><hr className="dropdown-divider border-top border-secondary" /></li>
+            <div className='btn-group row' role='types2' aria-label='Pokemon Types radio button group'>
+            { types.length !== 0 ? types.map((type) => (
+                <li key={type.name+"2"} className='col-2'>
+                    <input type="radio" className="btn-check" name="types2" id={type.name+"2"} autoComplete="off" />
+                    <label className={`btn btn-outline-${props.mode === 'light' ? 'dark' : 'secondary'} text-white btn-type`} htmlFor={type.name+"2"} title={typeNameF(type)}>
+                        <img src={imgUrl(type)} alt={typeNameF(type)} style={{width: '100%', height: '100%', objectFit: 'contain'}}/>
+                    </label>
+                </li>
+            )) : <h3 className={`text-${props.mode === 'light' ? 'dark' : 'white'}`}>Loading..</h3>}
+            </div>
+        </ul>
+    </div>
     </div>);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 import "./main.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
